6c9c9f23550cac1e4056b41fd79d6d0452b25ac5
m0jik <ajmueller25@gmail.com>
2025-11-20 16:56:57 -0600

    Set up test web stuff

--- App.go
+++ App.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type Page struct {
+    Title string
+    Body  []byte
 }
 
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := os.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+//Run program, go to http://localhost:8080/view/TestPage
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/view/", viewHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
